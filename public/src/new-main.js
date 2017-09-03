2485382637bc07dbfc5a4a8a7f27a61a115fbd1d
Michael Gutleber <michaeljgut@gmail.com>
2017-09-03 14:49:09 -0400

    Changed comment entry cancel link to go back to movie review show page

--- public/src/new-main.js
+++ public/src/new-main.js
@@ -2,3 +2,7 @@ let movieTitleInput = document.getElementById('movie_title');
 let commentInput = document.getElementById('comment');
 commentInput.focus();
 movieTitleInput.value = localStorage.getItem('movieTitle');
+let newAnchor = document.createElement('a');
+newAnchor.href = "/movie-reviews/" + movieTitleInput.value;
+newAnchor.innerText = "Cancel";
+document.body.appendChild(newAnchor);
--- routes/movie-reviews-routes.js
+++ routes/movie-reviews-routes.js
@@ -4,7 +4,9 @@ const movieReviewsRouter = express.Router();
 const movieReviewsController = require('../controllers/movie-review-controller');
 const movieReviewsHelper = require('../services/movie-reviews/movie-reviews-helper');
 
-movieReviewsRouter.get('/', movieReviewsController.index);
+movieReviewsRouter.get('/', movieReviewsHelper.getMovieReviewsFromAPI,
+  movieReviewsController.sendApiMovieReviews);
+// movieReviewsRouter.get('/', movieReviewsController.index);
 // movieReviewsRouter.get('/show', movieReviewsHelper.getMovieReviewsFromAPI,
 //   movieReviewsController.sendApiMovieReviews);
 movieReviewsRouter.post('/show', movieReviewsHelper.getMovieReviewsFromAPI,
